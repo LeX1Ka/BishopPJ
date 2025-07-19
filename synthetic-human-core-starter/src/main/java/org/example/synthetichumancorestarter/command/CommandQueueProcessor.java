2ad5895fc16e3502bae38bffd961711eaf7010c9
Alex Kalakutsky <shurik.kalakutsky@gmail.com>
2025-07-19 12:39:30 +0300

    feat(metrics): реализован счётчик выполненных команд по авторам

--- synthetic-human-core-starter/src/main/java/org/example/synthetichumancorestarter/command/CommandQueueProcessor.java
+++ synthetic-human-core-starter/src/main/java/org/example/synthetichumancorestarter/command/CommandQueueProcessor.java
@@ -1,14 +1,25 @@
 package org.example.synthetichumancorestarter.command;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
+import java.util.Map;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class CommandQueueProcessor {
     BlockingQueue<CommandRequest> queue = new ArrayBlockingQueue<>(100);
+    private final MeterRegistry meterRegistry;
+    private final Map<String, Counter> authorCounter = new ConcurrentHashMap<>();
+
+    public CommandQueueProcessor(MeterRegistry meterRegistry) {
+        this.meterRegistry = meterRegistry;
+        this.meterRegistry.gauge("queue.size", queue, BlockingQueue::size);
+    }
 
     public void enqueue(CommandRequest command) {
         boolean success = queue.offer(command);
@@ -25,6 +36,10 @@ public class CommandQueueProcessor {
                 try {
                     CommandRequest command = queue.take();
                     System.out.println("Выполнение COMMON команды: " + command.getDescription());
+
+                    String author = command.getAuthor();
+                    Counter counter = meterRegistry.counter("commands.executed.by.author", "author", author);
+                    counter.increment();
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
